cc4b528ccf2882ebdd9d04895d937568d382f41f
kalpa <476619022@qq.com>
2025-06-25 11:30:00 +0800

    modify github action 文件

--- src/main/java/com/kalpa/video/config/SpringSecurityConfig.java
+++ src/main/java/com/kalpa/video/config/SpringSecurityConfig.java
@@ -1,24 +1,18 @@
 package com.kalpa.video.config;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.web.SecurityFilterChain;
-
-@Configuration
-@EnableWebSecurity
-public class SpringSecurityConfig {
-
-    @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http
-                .authorizeHttpRequests((authorizeHttpRequests) ->
-                        authorizeHttpRequests
-//                                .requestMatchers("/**").hasRole("USER")
-//                                .requestMatchers("/admin/**").hasRole("ADMIN")
-                                .requestMatchers("/actuator/**").permitAll()
-                );
-        return http.build();
-    }
-}
+//@Configuration
+//@EnableWebSecurity
+//public class SpringSecurityConfig {
+//
+//    @Bean
+//    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+//        http
+//                .authorizeHttpRequests((authorizeHttpRequests) ->
+//                        authorizeHttpRequests
+////                                .requestMatchers("/**").hasRole("USER")
+////                                .requestMatchers("/admin/**").hasRole("ADMIN")
+//                                .requestMatchers("/actuator/**").permitAll()
+//                );
+//        return http.build();
+//    }
+//}
