1c7bba074589f442bec4de4f6ef562d5a7f65542
kalpa <476619022@qq.com>
2025-06-26 08:43:10 +0800

    修改count

--- src/main/java/com/kalpa/video/controller/IndexController.java
+++ src/main/java/com/kalpa/video/controller/IndexController.java
@@ -7,6 +7,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Random;
+import java.util.concurrent.atomic.AtomicInteger;
+
 @RestController
 @RequestMapping("/v1")
 public class IndexController {
@@ -14,13 +17,16 @@ public class IndexController {
     @Autowired
     MeterRegistry registry;
 
+
     private Counter counter_core;
     private Counter counter_index;
+    private AtomicInteger app_online_count;
 
     @PostConstruct
     private void init(){
         counter_core = registry.counter("app_requests_method_count", "method", "IndexController.core");
         counter_index = registry.counter("app_requests_method_count", "method", "IndexController.index");
+        app_online_count = registry.gauge("app_online_count", new AtomicInteger(0));
     }
 
     @RequestMapping(value = "/index")
@@ -41,5 +47,17 @@ public class IndexController {
             return e;
         }
         return counter_core.count() + " coreUrl Monitor by Prometheus.";
+    }
+
+    @RequestMapping(value = "/online")
+    public Object onlineCount(){
+        int people = 0;
+        try {
+            people = new Random().nextInt(2000);
+            app_online_count.set(people);
+        } catch (Exception e){
+            return e;
+        }
+        return "current online people: " + people;
     }
 }
